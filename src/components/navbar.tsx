c1ed28981da9c66d34a2c81a6313316c026ee942
Fokkos <jameswmarch2002@gmail.com>
2023-11-11 17:43:28 +0000

    added modal

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,9 +24,7 @@ export default function Navbar(): React.ReactNode {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <img className="h-8 w-auto"
-                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                   alt="Your Company"/>
+              <h1 className={"font-bold text-white"}>VERY SECURE WEBSITE</h1>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
