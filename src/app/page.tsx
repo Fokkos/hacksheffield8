9d2221aab42a70e1b554d999b7070fe46dcaed2d
Jack Sanders <jacksanders404@gmail.com>
2023-11-12 07:04:32 +0000

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,33 @@ import IntroModal from "@/components/IntroModal";
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className={'w-full md:w-1/2 mx-auto mt-24 flex flex-col items-center'}>
+      <h1 className="text-md text-gray-500 text-center">
+        NOTE: This website is far better experienced with sound on (make sure autoplay of audio is allowed)
+      </h1>
+      <ul>
+        <li>
+          <a href={'https://support.mozilla.org/en-US/kb/block-autoplay'} className={'text-sm text-gray-500 cursor-pointer underline hover:text-gray-700'}>
+            Where to find autoplay settings on Firefox
+          </a>
+        </li>
+        <li>
+          <a href={'https://nerdschalk.com/how-to-disable-autoplay-on-google-chrome/'} className={'text-sm text-gray-500 cursor-pointer underline hover:text-gray-700'}>
+            Where to find autoplay settings on Chrome
+          </a>
+        </li>
+        <li>
+          <a href={'https://memrise.zendesk.com/hc/en-us/articles/360015889117-How-can-I-allow-audio-to-auto-play-on-Safari-'} className={'text-sm text-gray-500 cursor-pointer underline hover:text-gray-700'}>
+            Where to find autoplay settings on Safari
+          </a>
+        </li>
+        <li>
+          <a href={'https://www.makeuseof.com/manage-autoplay-settings-microsoft-edge/'} className={'text-sm text-gray-500 cursor-pointer underline hover:text-gray-700'}>
+            Where to find autoplay settings on Microsoft Edge
+          </a>
+        </li>
+      </ul>
+
       <IntroModal />
     </main>
   )
